9cd6edfb14128be31e5513ae6beb5439aa24be4d
Alexandr Pogodin <alexandr.pogodin@saltedge.com>
2023-06-05 16:46:17 +0300

    Specify gem_binary for know platforms

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,12 @@
 package 'ruby'
 
 # Install example gem into the system
-gem_package 'hello-world'
+gem_package 'hello-world' do
+  gem_binary value_for_platform(
+    'debian' => {
+      '10'      => '/usr/bin/gem2.5',
+      '11'      => '/usr/bin/gem2.7',
+      'default' => '/usr/bin/gem'
+    }
+  )
+end
